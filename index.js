8b5c802c4eab2426822884817daf879f051caaa4
afifpratama <afifpratama@gmail.com>
2021-11-22 15:01:49 +0700

    pindah html ke public

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const app = require('express')();
+const express = require("express")
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-let env = require("./helper/env")
-let port = env.PORT;
+const env = require("./helper/env")
+const port = env.PORT;
+// const path = require('path');
+// const ui = path.join(__dirname, './public');
+
+let { userouter } = require('./router')
+// app.use(express.static(ui))
+app.use("/ngetes", userouter)
+
+http.listen(port, () => console.log(port))
 
 app.get('/', (req, res) => {
     io.on('connection', (socket) => {
@@ -11,7 +20,7 @@ app.get('/', (req, res) => {
         });
     });
 
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 io.on('connection', (socket) => {
@@ -19,12 +28,6 @@ io.on('connection', (socket) => {
         console.log('message: ' + msg);
     });
 });
-
-let { userouter } = require('./router')
-
-app.use("/ngetes", userouter)
-
-http.listen(port, () => console.log(port))
 // const http = require('http');
 // const server = http.createServer(app);
 // const { Server } = require("socket.io");
